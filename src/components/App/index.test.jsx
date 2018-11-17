0ba38842ac97578ab44298afd226e58fb2f3c958
Jan Grzegorowski <grzegorowski@gmail.com>
2018-11-17 17:18:28 +0100

    test: add simple test case for App component

--- src/components/App/index.test.jsx
+++ src/components/App/index.test.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './index';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-  expect(1).toBe(1);
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('.App').text()).toBe('OK');
 });
