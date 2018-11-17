89a239ce9c677b65b322db8557a8f0bd3fbb821b
Jan Grzegorowski <grzegorowski@gmail.com>
2018-11-17 17:13:48 +0100

    feat: reorganize initial files structure and remove serviveWorker

---
+++ src/components/App/index.jsx
@@ -0,0 +1,12 @@
+// @flow
+import React from 'react';
+
+type Props = {};
+
+class App extends React.Component<Props> {
+  render() {
+    return <div className="App">OK</div>;
+  }
+}
+
+export default App;
