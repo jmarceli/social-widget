867822e14744ccc1d9091112eebb9044ea7e11ea
Jan Grzegorowski <grzegorowski@gmail.com>
2018-11-28 11:17:19 +0100

    chore: add rerender support in test-utils

--- utils/test-utils.js
+++ utils/test-utils.js
@@ -4,7 +4,15 @@ import { ThemeProvider } from 'react-jss';
 import theme from '../src/theme';
 
 const customRender = (node, options) => {
-  return render(<ThemeProvider theme={theme}>{node}</ThemeProvider>, options);
+  const rendered = render(
+    <ThemeProvider theme={theme}>{node}</ThemeProvider>,
+    options,
+  );
+  return {
+    ...rendered,
+    rerender: (ui, options) =>
+      customRender(ui, { container: rendered.container, ...options }),
+  };
 };
 
 // re-export everything
