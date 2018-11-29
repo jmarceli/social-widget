3527dca4073325425b2db37006ac37f61afbd7f5
Jan Grzegorowski <grzegorowski@gmail.com>
2018-11-29 16:19:10 +0100

    test[Comments]: convert from enzyme to rtl

--- src/components/Comments/index.test.jsx
+++ src/components/Comments/index.test.jsx
@@ -1,44 +1,76 @@
 import React from 'react';
-import CommentsStyled, { Comments } from './index';
-import CommentList from './CommentList';
-import CommentForm from './CommentForm';
-import { shallow, render } from 'enzyme';
-import theme from '../../theme';
-import { ThemeProvider } from 'react-jss';
-import Transition from 'react-transition-group/Transition';
+import Comments from './index';
+import { render, fireEvent } from 'test-utils';
+import { animateScroll } from 'react-scroll';
 
 const data = {
   isHidden: false,
-  list: [{}, {}, {}],
+  list: new Array(10).fill({}).map((item, index) => ({
+    isLoading: false,
+    imgSrc: './test.jpg',
+    author: `Test ${index + 1}`,
+    content: `Test content ${index + 1}`,
+    pubTimestamp: 1543420231093,
+  })),
   handleHide: jest.fn(),
   handleAdd: jest.fn(),
 };
 
 describe('<Comments />', () => {
-  test('rendering', () => {
-    const wrapper = render(
-      <ThemeProvider theme={theme}>
-        <CommentsStyled {...data} />
-      </ThemeProvider>,
+  test('loading state', () => {
+    const { baseElement } = render(<Comments {...data} isLoading={true} />);
+    expect(baseElement.getElementsByTagName('svg').length).toBe(
+      1 + data.list.length,
     );
-    expect(wrapper.text()).toContain('Hide comments (3)');
   });
-});
 
-describe('<Comments /> shallow', () => {
-  let wrapper;
-  beforeAll(() => {
-    wrapper = shallow(<Comments classes={{}} {...data} />);
+  test('rendering when expanded', () => {
+    data.handleHide.mockClear();
+    const { getByText } = render(<Comments {...data} isLoading={false} />);
+    const button = getByText('Hide comments (10)');
+    expect(button).toBeDefined();
+    fireEvent.click(button);
+    expect(data.handleHide).toBeCalledTimes(1);
   });
-  test('toggle button existence', () => {
-    const button = wrapper.find('button');
-    expect(button.length).toBe(1);
+
+  test('rendering when collapsed', () => {
+    data.handleHide.mockClear();
+    const { getByText } = render(
+      <Comments {...data} isLoading={false} isHidden={true} />,
+    );
+    const button = getByText('Show comments (10)');
+    expect(button).toBeDefined();
+    fireEvent.click(button);
+    expect(data.handleHide).toBeCalledTimes(1);
   });
-  test('<CommentForm/> and <CommentList/> shows after toggle', () => {
-    const button = wrapper.find('button');
-    button.simulate('click');
-    const transition = wrapper.find(Transition).dive();
-    expect(transition.find('Jss(CommentForm)').length).toBe(1);
-    expect(transition.find('Jss(CommentList)').length).toBe(1);
+
+  test('auto scroll on init', () => {
+    // TODO: write better scrolling tests, currently there is no guarantee that element is
+    // actually scrolled, just that scrollToBottom method was invoked
+    const scrollSpy = jest.spyOn(animateScroll, 'scrollToBottom');
+    render(<Comments {...data} isLoading={false} isHidden={false} />);
+    expect(scrollSpy).toBeCalledTimes(1);
+    scrollSpy.mockRestore();
+    // const scroller = getByTestId('scroller');
+    // console.log('test');
+    // expect(scroller.scrollTop).toBeGreaterThan(0);
+  });
+
+  test('auto scroll after adding new comment', () => {
+    // TODO: write better scrolling tests, currently there is no guarantee that element is
+    // actually scrolled, just that scrollToBottom method was invoked
+    const { baseElement, getByLabelText } = render(
+      <Comments {...data} isLoading={false} isHidden={false} />,
+    );
+
+    const scrollSpy = jest.spyOn(animateScroll, 'scrollToBottom');
+    const input = getByLabelText('Add a comment');
+    fireEvent.change(input, { target: { value: 'Some comment' } });
+
+    // this should be fixed somehow, but there is a problem with simulating ENTER
+    // keypress inside input field (it doesn't trigger form submission in tests
+    fireEvent.submit(baseElement.getElementsByTagName('form')[0]);
+    expect(scrollSpy).toBeCalledTimes(1);
+    scrollSpy.mockRestore();
   });
 });
